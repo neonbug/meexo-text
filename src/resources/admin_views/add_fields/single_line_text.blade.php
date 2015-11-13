e47c650498a093a386874c06d003b234689052a8
Tadej Kanižar <tadej@ncode.si>
2015-11-13 11:23:16 +0100

    Initial commit

---
+++ src/resources/admin_views/add_fields/single_line_text.blade.php
@@ -0,0 +1,11 @@
+<tr class="top aligned field-single-line-text">
+	<th class="collapsing">
+		A{{ $field_title }}A
+	</th>
+	<td>
+		<div class="field">
+			<input type="text" name="field[{{ $id_language }}][{{ $field['name'] }}]" value="{{ $field['value'] }}"
+				data-name="{{ $field['name'] }}" />
+		</div>
+	</td>
+</tr>
